eaa239b47eef9877fe8d7482f0c01bbef491d98f
Arthur Correnson <arthur.correnson@gmail.com>
2021-10-25 22:41:33 +0200

    tiny proof checker

--- src/Clauses.v
+++ src/Clauses.v
@@ -305,7 +305,6 @@ Qed.
 
 End ClauseSet.
 
-
 Inductive resolvent : ClauseSet.t -> Clause.t -> Prop :=
   | resolvent_mem : forall cs c,
     ClauseSet.mem cs c = true -> resolvent cs c
@@ -334,7 +333,7 @@ Theorem resolvent_sound :
 Proof.
   intros m cs c Hres Heval.
   induction Hres.
-  + firstorder using clause_set_clause_true.
+  + apply (ClauseSet.eval_true_forall cs m Heval c H).
   + pose proof (Hl1 := ClauseSet.eval_true_forall cs m Heval (l1 :: c1) H).
     pose proof (Hl2 := ClauseSet.eval_true_forall cs m Heval (l2 :: c2) H0).
     simpl in Hl1, Hl2.
@@ -363,9 +362,110 @@ Proof.
         now destruct (satisfy m n0).
 Qed.
 
+Definition sat (cs : ClauseSet.t) :=
+  exists m, ClauseSet.eval m cs = true.
 
+Definition unsat (cs : ClauseSet.t) :=
+  ~sat cs.
 
+Theorem resolvent_bot_unsat :
+  forall cs, resolvent cs [] -> unsat cs.
+Proof.
+  intros cs Hres [m Hm].
+  now pose proof (resolvent_sound m cs _ Hres Hm).
+Qed.
 
+Inductive resolvent_seq : ClauseSet.t -> Clause.t -> Prop :=
+  | resolvent_seq_base : forall cs c,
+    resolvent cs c -> resolvent_seq cs c
+  | resolvent_seq_step : forall cs c c',
+    resolvent cs c ->
+    resolvent_seq (c::cs) c' ->
+    resolvent_seq cs c'.
 
+Lemma resolvent_seq_sound :
+  forall cs c m,
+    resolvent_seq cs c ->
+    ClauseSet.eval m cs = true ->
+    Clause.eval m c = true.
+Proof.
+  intros cs c m Hres Heval.
+  induction Hres.
+  + eauto using resolvent_sound.
+  + eapply IHHres.
+    simpl; apply Bool.andb_true_iff; split; auto.
+    eauto using resolvent_sound.
+Qed.
 
+Inductive proof_step : Type :=
+  | proof_mem (c : Clause.t)
+  | proof_cut (n : nat) (c1 c2 : Clause.t).
 
+Definition conclusion (ut : proof_step) : Clause.t :=
+  match ut with
+  | proof_mem c => c
+  | proof_cut _ c1 c2 => c1 ++ c2
+  end.
+
+Definition is_proof_step (context : ClauseSet.t) (ut : proof_step) : bool :=
+  match ut with
+  | proof_mem c =>
+    ClauseSet.mem context c
+  | proof_cut l c1 c2 =>
+    ClauseSet.mem context (Literal.Neg l::c1) &&
+    ClauseSet.mem context (Literal.Pos l::c2)
+  end.
+
+Lemma is_proof_step_sound' :
+  forall ctx ut,
+    is_proof_step ctx ut = true ->
+    resolvent ctx (conclusion ut).
+Proof.
+  intros ctx ut.
+  induction ut as [ | l c1 c2 ].
+  - eauto using resolvent.
+  - intros [H1 H2]%Bool.andb_true_iff.
+    eauto using resolvent.
+Qed.
+
+Lemma is_proof_step_sound :
+  forall ctx ut m,
+    is_proof_step ctx ut = true ->
+    ClauseSet.eval m ctx = true ->
+    Clause.eval m (conclusion ut) = true.
+Proof.
+  eauto using resolvent_sound, is_proof_step_sound'.
+Qed.
+
+Fixpoint is_proof (context : ClauseSet.t) (uts : list proof_step) (c : Clause.t) : bool :=
+  match uts with
+  | [] => ClauseSet.mem context c
+  | ut::uts =>
+    is_proof_step context ut &&
+    is_proof ((conclusion ut)::context) uts c
+  end.
+
+Lemma is_proof_sound' :
+  forall uts ctx c,
+  is_proof ctx uts c = true ->
+  resolvent_seq ctx c.
+Proof.
+  intros uts.
+  induction uts.
+  - eauto using resolvent_seq_base, resolvent_mem.
+  - intros. simpl in H.
+    rewrite Bool.andb_true_iff in H.
+    destruct H as [H1 H2].
+    eapply resolvent_seq_step.
+    + apply (is_proof_step_sound' _ _ H1).
+    + apply (IHuts _ _ H2).
+Qed.
+
+Lemma is_proof_sound :
+  forall uts ctx c m,
+    is_proof ctx uts c = true ->
+    ClauseSet.eval m ctx = true ->
+    Clause.eval m c = true.
+Proof.
+  eauto using resolvent_seq_sound, is_proof_sound'.
+Qed.
